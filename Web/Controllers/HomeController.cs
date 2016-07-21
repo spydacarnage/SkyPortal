2c239685b4ae2675ec649638ed76f6baf4b4c57c
Mark Quennell <spydacarnage@live.com>
2016-07-21 09:31:07 +0100

    With Link

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -6,11 +6,15 @@ using System.Web.Mvc;
 
 namespace SkyInsurance.SkyPortal.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return Content($"Hello there. The clock is {DateTime.UtcNow}");
-		}
-	}
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string output = $"Hello there. The clock is {DateTime.UtcNow}";
+            output += "<br/><br/>";
+            output += "<a href=\"/TCAS/Temp\">Temp Input</a>";
+
+            return Content(output);
+        }
+    }
 }
